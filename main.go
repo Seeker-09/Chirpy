472c300744c7b45f4c43a4822cd0bf3f0c4791b2
David Mundt <54086931+Seeker-09@users.noreply.github.com>
2025-05-13 11:14:13 -0400

    middleware

--- main.go
+++ main.go
@@ -4,20 +4,47 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+}
 
 func main() {
 	mux := http.NewServeMux()
+	apiCfg := &apiConfig{}
 
 	fs := http.FileServer(http.Dir("."))
-	mux.Handle("/app/assets/", fs)
-	mux.Handle("/app/", fs)
+	mux.Handle("/app/assets/", apiCfg.middlewareMetricsInc(fs))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fs))
 
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
+
+	mux.HandleFunc("/metrics", apiCfg.handleMetrics)
+	mux.HandleFunc("/reset", apiCfg.handleReset)
 
 	server := &http.Server{
 		Addr:    ":8080",
